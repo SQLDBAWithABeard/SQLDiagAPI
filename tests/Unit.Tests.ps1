692ce4576fc2e2dd98097033f3aebd30255ec4c0
Rob Sewell <mrrobsewell@outlook.com>
2017-06-27 13:06:52 +0100

    Added Test for no environment variable

--- tests/Unit.Tests.ps1
+++ tests/Unit.Tests.ps1
@@ -1,8 +1,45 @@
-$projectRoot = Resolve-Path "$PSScriptRoot\.."
 $script:ModuleName = 'SQLDiagAPI'
 # Removes all versions of the module from the session before importing
 Get-Module $ModuleName | Remove-Module
-Import-Module $ModuleBase\$ModuleName.psd1 -PassThru -ErrorAction Stop
-Describe "Basic function unit tests" -Tags Build {
+$ModuleBase = Split-Path -Parent $MyInvocation.MyCommand.Path
+# For tests in .\Tests subdirectory
+if ((Split-Path $ModuleBase -Leaf) -eq 'Tests') {
+    $ModuleBase = Split-Path $ModuleBase -Parent
+}
 
+Import-Module $ModuleBase\$ModuleName.psd1 -PassThru -ErrorAction Stop | Out-Null
+Describe "Get-SQLDiagRecommendations" -Tags Build , Unit{
+    Context "Requirements" {
+        Mock Get-ChildItem {}
+        Mock Write-Warning {"Warning"}
+        It "Should throw a warning if there is no API Key Environment Variable"{
+            Get-SQLDiagRecommendations -ErrorAction SilentlyContinue | Should Be "Warning"
+        }
+        It 'Checks the Mock was called for Get-ChildItem' {
+            $assertMockParams = @{
+                'CommandName' = 'Get-ChildItem'
+                'Times'       = 1
+                'Exactly'     = $true
+            }
+            Assert-MockCalled @assertMockParams 
+        }
+        It 'Checks the Mock was called for Write-Warning' {
+            $assertMockParams = @{
+                'CommandName' = 'Write-Warning'
+                'Times'       = 1
+                'Exactly'     = $true
+            }
+            Assert-MockCalled @assertMockParams 
+        }
+
+    }
+    Context "Input" {
+
+    }
+    Context "Execution" {
+
+    }
+    Context "Output" {
+        
+    }
 }
