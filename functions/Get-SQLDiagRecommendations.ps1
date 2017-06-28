6196e3dad78bd64c93002de8e2e96f68a072448d
Rob Sewell <mrrobsewell@outlook.com>
2017-06-28 13:52:20 +0100

    APIKey Recommendations

--- functions/Get-SQLDiagRecommendations.ps1
+++ functions/Get-SQLDiagRecommendations.ps1
@@ -1,8 +1,16 @@
 function Get-SQLDiagRecommendations {
-
-if(!(Get-ChildItem Env:MS_SQLDiag_APIKey -ErrorAction SilentlyContinue)){
-        Write-Warning "You have not created an Environment Variable MS_SQLDiag_APIKey to hold the APIKey
-         You can do this using [Environment]::SetEnvironmentVariable(`"MS_SQLDiag_APIKey`", `"APIKEYGOESHERE`", `"User`") 
+    [cmdletbinding()]
+    Param([string]$ApiKey)
+    if (!$ApiKey) {
+        if (!(Test-Path "${env:\userprofile}\SQLDiag.Cred")) {
+            Write-Warning "You have not created an XML File to hold the API Key or provided the API Key as a parameter
+         You can export the key to an XML file using Get-Credential | Export-CliXml -Path `"`${env:\userprofile}\SQLDiag.Cred`"
          You can get a key by following the steps here https://ecsapi.portal.azure-api.net/ "
-}
+        }
+        else {
+            $APIKey = (Import-Clixml -Path "${env:\userprofile}\SQLDiag.Cred").GetNetworkCredential().Password
+        }
+    }
+
+
 }
