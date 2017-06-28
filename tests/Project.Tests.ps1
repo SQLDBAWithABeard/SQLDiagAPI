728014b7e8730c27f9133b832f7e5cf1347640ef
Rob Sewell <mrrobsewell@outlook.com>
2017-06-28 15:32:22 +0100

    altered to use the exceptions

--- tests/Project.Tests.ps1
+++ tests/Project.Tests.ps1
@@ -2,23 +2,25 @@ $script:ModuleName = 'SQLDiagAPI'
 # Removes all versions of the module from the session before importing
 Get-Module $ModuleName | Remove-Module
 $ModuleBase = Split-Path -Parent $MyInvocation.MyCommand.Path
+$FunctionHelpTestExceptions = Get-Content -Path "$ModuleBase\Help.Exceptions.ps1"
 # For tests in .\Tests subdirectory
 if ((Split-Path $ModuleBase -Leaf) -eq 'Tests') {
     $ModuleBase = Split-Path $ModuleBase -Parent
 }
-
 Import-Module $ModuleBase\$ModuleName.psd1 -PassThru -ErrorAction Stop | Out-Null
 Describe "PSScriptAnalyzer rule-sets" -Tag Build, ScriptAnalyzer {
 
-    $Rules = Get-ScriptAnalyzerRule
+    $Rules = Get-ScriptAnalyzerRule 
     $scripts = Get-ChildItem $ModuleBase -Include *.ps1, *.psm1, *.psd1 -Recurse | Where-Object fullname -notmatch 'classes'
 
     foreach ( $Script in $scripts ) 
     {
         Context "Script '$($script.FullName)'" {
 
-            foreach ( $rule in $rules )
+            foreach ( $rule in $rules)
             {
+                # Skip all rules that are on the exclusions list
+                if ($FunctionHelpTestExceptions -contains $rule.RuleName) { continue }
                 It "Rule [$rule]" {
 
                     (Invoke-ScriptAnalyzer -Path $script.FullName -IncludeRule $rule.RuleName ).Count | Should Be 0
