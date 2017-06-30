400e99a9975fb7ae6257076e692e49e28cde49e4
Rob Sewell <mrrobsewell@outlook.com>
2017-06-30 17:55:38 +0100

    get-sqldiagfeatures no parameter

--- functions/Get-SQLDiagFeature.ps1
+++ functions/Get-SQLDiagFeature.ps1
@@ -7,5 +7,14 @@ function Get-SQLDiagFeature {
         [ValidateNotNull()]
         [pscustomobject] $Recommendations = (Get-SQLDiagRecommendations)
     )
-$Recommendations
+    $features = @()
+    foreach ($recommendation in $recommendations.Recommendations) {
+        foreach ($fix in $recommendation.Content.RelevantFixes) {
+            $feature = $fix.Title
+            if ($features -notcontains $feature) {
+                $Features += $Feature
+            }
+        }
+    }
+    $features | Sort-Object
 }
