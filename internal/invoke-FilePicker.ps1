d2b676ae154da27ee3cf667a747c43783b51c77d
Rob Sewell <mrrobsewell@outlook.com>
2017-07-07 14:37:29 +0100

    Added Invoke-FilePicker nad ShowDialog

---
+++ internal/invoke-FilePicker.ps1
@@ -0,0 +1,24 @@
+function Invoke-FilePicker {
+    ##############################
+    #.SYNOPSIS
+    # Internal command to open a file browser and select a mdmp file and return Path and Size in MB
+    ##############################
+    Add-Type -AssemblyName System.Windows.Forms
+    $Picker = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+        Multiselect = $false # Multiple files can be chosen
+        Filter      = 'SQL Server Dump File (*.mdmp)|*.mdmp'
+    }
+
+    ShowDialog 
+    
+    $File = Get-Item $Picker.FileName
+    [PSCustomObject] @{
+        FileName = $File.Fullname
+        FileSize = $File.Length / 1MB
+    }
+
+}
+
+
+
+
