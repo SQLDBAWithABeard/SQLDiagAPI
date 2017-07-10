aea28fbbf172bb45e07a67e94b03b18a94c62852
Rob Sewell <mrrobsewell@outlook.com>
2017-07-10 10:40:16 +0100

    added rough get dump file

--- functions/Get-SQLDiagDumpFile.ps1
+++ functions/Get-SQLDiagDumpFile.ps1
@@ -1,4 +1,30 @@
+<#
+.SYNOPSIS
+Returns the File Path and Size in Mb of the dump file
 
+.DESCRIPTION
+Returns the File Path and Size in Mb of the dump file
+for uploading to the SQL Server Diagnostics Recommendations API
+
+.PARAMETER file
+The Path to the dump file - If not specfied a file picker 
+dialogue will be shown
+
+.EXAMPLE
+Get-SQLDiagDumpFile
+
+Opens a file picker diaglogue and returns the File Path and Size in Mb 
+of the chosen file
+
+.EXAMPLE
+Get-SQLDiagDumpFile -File C:\SQLServer\Log\SQLDump011.mdmp
+
+Returns the File Path and Size in Mb of the specified file
+
+.NOTES
+    AUTHOR  Rob Sewell @SQLDBAWithBeard https://sqldbawithabeard.com
+    DATE    07/07/2017
+#>
 function Get-SQLDiagDumpFile {
     param(
         [parameter(ValueFromPipelineByPropertyName, 
@@ -18,7 +44,7 @@ function Get-SQLDiagDumpFile {
 
         [pscustomobject]@{
             FilePath = $DumpFile.FullName
-            Size     = $DumpFile.Length /1MB
+            Size     = $DumpFile.Length
         }
     }
     End{}
