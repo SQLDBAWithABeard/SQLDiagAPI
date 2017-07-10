b9ddee27243ce80f191a36a82e3d5bddd0d58fb6
Rob Sewell <mrrobsewell@outlook.com>
2017-07-10 11:56:44 +0100

    added dump analysis

--- functions/Invoke-SQLDiagDumpAnalysis.ps1
+++ functions/Invoke-SQLDiagDumpAnalysis.ps1
@@ -1,3 +1,35 @@
+<#
+.SYNOPSIS
+Uploads a dump file to the SQL Server Diagnostics API for Analysis
+
+.DESCRIPTION
+Uploads a dump file to the SQL Server Diagnostics API for Analysis
+Details here https://ecsapi.portal.azure-api.net/
+
+It requires the APIKey parameter or the APIKey to be stored using Export-CliXML in the users profile
+in a file named SQLDiag.Cred
+
+.PARAMETER File
+The Fiile Path or File object of the dump file to be analysed
+
+.PARAMETER ApiKey
+The APIKey used to authenticate against the API. You can get one from https://ecsapi.portal.azure-api.net/
+
+.PARAMETER Region
+The Azure Region to upload the file to. Get-SQLDiagSupportedRegions shows the available regions
+
+.PARAMETER Email
+The email address to receive notification that analysis has completed and the recommendations
+
+.EXAMPLE
+Invoke-SQLDiagDumpAnalysis -File c:\temp\SQLDump073.mdmp -Region 'West Us' -Email 'a@a.com'
+
+Uploads the file to the API and emails when it has completed
+
+.NOTES
+    AUTHOR  Rob Sewell @SQLDBAWithBeard https://sqldbawithabeard.com
+    DATE    10/07/2017
+#>
 function Invoke-SQLDiagDumpAnalysis {
     [cmdletbinding(SupportsShouldProcess = $true)]
     param(
@@ -42,9 +74,8 @@ function Invoke-SQLDiagDumpAnalysis {
             try {
                 if ($PSCmdlet.ShouldProcess($env:COMPUTERNAME, "Get information about $File")) { 
                     Get-SQLDiagDumpFile -file $File
-                    }
-                    Write-Verbose "Got File Information FileName = $File.FilePath Fie Size = $file.FileSize"
                 }
+                Write-Verbose "Got File Information FileName = $File.FileName File Size = $file.FileSize"
             }
             catch {
                 Write-Warning -Message "Failed to get File Information about $File - Quitting"
@@ -52,10 +83,8 @@ function Invoke-SQLDiagDumpAnalysis {
                 break
             }
         }
-
     }
     Process {
-
         try {
             Write-Verbose "Get the UploadURL from the API"
             if ($PSCmdlet.ShouldProcess($($File.FullPath), "Get the UploadURL from the API")) { 
@@ -69,48 +98,35 @@ function Invoke-SQLDiagDumpAnalysis {
         }
         $RequestID = $UploadResponse.RequestID
         $UploadURL = $UploadResponse.UploadURL
-        try{
-        Write-verbose -Message "Upload the File to storage for analysis"
-        if ($PSCmdlet.ShouldProcess($($File.FullPath), "Upload the File to storage for analysis")) { 
-                Invoke-FileUpload -Uri $UploadURL -File $File.FullPath -FileSize $File.Size
+        try {
+            Write-verbose -Message "Upload the File to storage for analysis"
+            if ($PSCmdlet.ShouldProcess($($File.FullPath), "Upload the File to storage for analysis")) { 
+                Start-FileUpload -Uri $UploadURL -File $File.FullPath -FileSize $File.Size
+            }
         }
-        catch{
-                Write-Warning -Message "Failed to upload the File $($File.FullPath) for RequestID $RequestID"
+        catch {
+            Write-Warning -Message "Failed to upload the File $($File.FullPath) for RequestID $RequestID"
             break
         }
-            $InitiateAnalysisAPIURL = 'https://ecsapi.azure-api.net/DiagnosticAnalysis/SQLAnalysis/InitiateAnalysis'
-
-            $headers = @{ "Ocp-Apim-Subscription-Key" = $apiKey }        
-            $Body = 
-            @{
-                clientId  = $MachineGUID
-                RequestID = $RequestID
-            } | ConvertTo-Json
-            Write-Verbose -Message "Intiating Analysis of Dump File $File"
-            if ($PSCmdlet.ShouldProcess('SQL Analysis Initiate Analysis', "Intiating Analysis of Dump File $File with $InitiateAnalysisAPIURL")) { 
-                try {
-                    $response = Invoke-RestMethod -Method Post -Uri $InitiateAnalysisAPIURL -Headers $headers -Body $Body -ContentType "application/json"  -ErrorAction Stop
-                    Write-Verbose -Message "Intiated Analysis of Dump File $File - waiting for response"
-                }
-                catch {
-                    Write-Warning -Message "Failed to initiate analysis of $Fiel with API $InitiateAnalysisAPIURL" 
-                    break
-                }
+        Write-Verbose -Message "Intiating Analysis of Dump File $File"
+        try {
+            if ($PSCmdlet.ShouldProcess('SQL Analysis Initiate Analysis', "Intiating Analysis of Dump File $File")) { 
+            
+                Start-FileAnalysis -APIKey $ApiKey -MachineGUID $MachineGUID -RequestID $RequestID
             }
         }
-        End {
-
-            If ($response) {
-                Write-Verbose "Success"
-            } 
-            else {
-                Write-Warning "Failed"
-            }
+        catch {
+            Write-Warning -Message "File analysis Initiation for $File Failed"
+            break
         }
-
-    
     }
-    #$MachineGUID = "f8fa8213-e978-424d-b939-5afa37cb2144"
-    ## $file = [PSCustomObject]@{FilePath = 'C:\temp\database.json'
-    ##     Size                           = 100
-    ## }
+    End {
+
+        If ($response) {
+            Write-Verbose "Successfully invoked Analysis of file $File.FullName with RequestID $RequestID"
+        } 
+        else {
+            Write-Warning "Failed to Invoke analysis of File $File.FullName"
+        }
+    }
+}
