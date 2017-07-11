4462e9882f144092cd42fe8f79a05c2c9f350623
Rob Sewell <mrrobsewell@outlook.com>
2017-07-11 10:17:08 +0100

    verbose

--- functions/Get-SQLDiagRecommendations.ps1
+++ functions/Get-SQLDiagRecommendations.ps1
@@ -34,6 +34,7 @@ function Get-SQLDiagRecommendations {
     [cmdletbinding()]
     Param([string]$ApiKey)
     if (!$ApiKey) {
+        Write-Verbose -Message "Getting the APIKey"
         if (!(Test-Path "${env:\userprofile}\SQLDiag.Cred")) {
             Write-Warning "You have not created an XML File to hold the API Key or provided the API Key as a parameter
          You can export the key to an XML file using Get-Credential | Export-CliXml -Path `"`${env:\userprofile}\SQLDiag.Cred`"
@@ -42,9 +43,10 @@ function Get-SQLDiagRecommendations {
         }
         else {
             $APIKey = (Import-Clixml -Path "${env:\userprofile}\SQLDiag.Cred").GetNetworkCredential().Password
+            Write-Verbose -Message "Got the API Key $APIKey"
         }
     }
-
+    Write-Verbose -Message "Getting the Machine GUID"
     $MachineGUID = Get-MachineGUID
 
     if($MachineGUID.length -eq 0)
@@ -52,9 +54,16 @@ function Get-SQLDiagRecommendations {
         Write-Warning "Failed to get Machine GUID from HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography\"
         break
     }
-
+    Write-Verbose -Message "Getting the LatestCUs from the API"
     $apiUrl = "https://ecsapi.azure-api.net/SQLServer/recommendations/latestcus/" + $MachineGUID + "?api-version=2017-06-01"
     $headers = @{ "Ocp-Apim-Subscription-Key" = $apiKey }
-
+    try {
+        if ($PSCmdlet.ShouldProcess($apiUrl, "Connecting to API to get Latest CUs")) { 
     Invoke-RestMethod -Method Get -Uri $apiUrl -Headers $headers -ContentType "application/json"  -ErrorAction Stop
+        }
+    }
+    catch {
+        Write-Warning " Failed to get Latest CUs from the API $APIURL"
+    }
+    Write-Verbose -Message "Got the Latest CUs"
 }
