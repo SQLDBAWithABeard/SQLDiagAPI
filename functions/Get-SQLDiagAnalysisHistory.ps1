d0035de0a3d1250b1ea8b57388df11a6f1c88281
Rob Sewell <mrrobsewell@outlook.com>
2017-07-11 12:49:29 +0100

    analysis history verbose and help

--- functions/Get-SQLDiagAnalysisHistory.ps1
+++ functions/Get-SQLDiagAnalysisHistory.ps1
@@ -1,12 +1,86 @@
+<#
+.SYNOPSIS
+Returns the SQL Server dump file diagnosis history for this machine from the 
+SQL Server Diagnostics API
+
+.DESCRIPTION
+Returns the SQL Server dump file diagnosis history for this machine from the 
+SQL Server Diagnostics API
+Details here https://ecsapi.portal.azure-api.net/
+
+It requires the APIKey parameter or the APIKey to be stored using Export-CliXML in the users profile
+in a file named SQLDiag.Cred
+
+.PARAMETER Status
+Status of the request - accepted values are Complete, Failed, In Progress
+
+.PARAMETER Complete
+Filter by Has the analysis completed requires $true or $false
+
+.PARAMETER Since
+Time paramater to filter the history - accepted values 'Today', 'Yesterday', 'This Week', 'This Month'
+
+.PARAMETER APIKey
+The APIKey used to authenticate against the API. You can get one from https://ecsapi.portal.azure-api.net/
+
+.EXAMPLE
+Get-SQLDiagAnalysisHistory
+
+Returns the SQL Server dump file diagnosis history for this machine from the 
+SQL Server Diagnostics API
+
+.EXAMPLE
+Get-SQLDiagAnalysisHistory -Status Complete
+
+Returns the SQL Server dump file diagnosis history for completed dump Analysis for this machine from the 
+SQL Server Diagnostics API
+
+.EXAMPLE
+Get-SQLDiagAnalysisHistory -Status Failed
+
+Returns the SQL Server dump file diagnosis history for dump Analysis that have failed for this machine from the 
+SQL Server Diagnostics API
+
+.EXAMPLE
+Get-SQLDiagAnalysisHistory -Status 'In Progress'
+
+Returns the SQL Server dump file diagnosis history for dump Analysis that are in progress for this machine from the 
+SQL Server Diagnostics API
+
+.EXAMPLE
+Get-SQLDiagAnalysisHistory -Since Today
+
+Returns the SQL Server dump file diagnosis history for dump Analysis since midnight today for this machine from the 
+SQL Server Diagnostics API
+
+.EXAMPLE
+Get-SQLDiagAnalysisHistory -Since Yesterday
+
+Returns the SQL Server dump file diagnosis history for dump Analysis since midnight Yesterday for this machine from the 
+SQL Server Diagnostics API
+
+.EXAMPLE
+Get-SQLDiagAnalysisHistory -Since 'This Week'
+
+Returns the SQL Server dump file diagnosis history for dump Analysis since midnight on Monday morning of this week for 
+this machine from the SQL Server Diagnostics API
+
+.EXAMPLE
+Get-SQLDiagAnalysisHistory -Since 'This Month'
+
+Returns the SQL Server dump file diagnosis history for dump Analysis since midnight on the 1st of this month for 
+this machine from the SQL Server Diagnostics API
+
+.NOTES
+AUTHOR  Rob Sewell @SQLDBAWithBeard https://sqldbawithabeard.com
+    DATE    28/06/2017
+#>
 function Get-SQLDiagAnalysisHistory {
     [cmdletbinding(SupportsShouldProcess = $true)]
     param( 
         [parameter(Mandatory = $false)]
-        [ValidateSet('In Progress', 'Failed', 'Suceeded')]
-        [string]$status,
-        [parameter(Mandatory = $false)]
-        [ValidateSet($true, $False)]
-        [string]$complete,
+        [ValidateSet('In Progress', 'Failed', 'Complete')]
+        [string]$Status,
         [parameter(Mandatory = $false)]
         [ValidateSet('Today', 'Yesterday', 'This Week', 'This Month')]
         $Since,
@@ -29,6 +103,13 @@ function Get-SQLDiagAnalysisHistory {
                 Write-Verbose -Message "Using the APIKEY $APIKey"
             }
         }
+        Write-Verbose -Message "Getting the Machine GUID"
+        $MachineGUID = Get-MachineGUID
+
+        if ($MachineGUID.length -eq 0) {
+            Write-Warning "Failed to get Machine GUID from HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography\"
+            break
+        }
         $AnalysisHistoryUrl = 'https://ecsapi.azure-api.net/DiagnosticAnalysis/SQLAnalysis/GetAnalysisHistory/' + $MachineGUID
         $headers = @{ "Ocp-Apim-Subscription-Key" = $apiKey }
         <#
@@ -45,19 +126,15 @@ put a swithc here for the inputs to be abel to combine them
             } 
         }
         catch {
-            Write-Warning "Failed to get the Analysis History form the API"
+            Write-Warning "Failed to get the Analysis History from the API"
             Write-Warning -Message "Analysis History URL = $AnalysisHistoryUrl" 
             Write-Warning -Message "Headers = $headers" 
             Write-Warning -Message "Body = $Body" 
+            break
         }
         if ($status) {
             Write-Verbose -Message "Filtering By Status $Status"
             $response | Where-Object {$_.RequestStatus -eq $status}
-        }
-        elseif ($complete) {
-            Write-Verbose -Message "Filtering By Completion $Complete"
-            $response | Where-Object {$_.IsComplete -eq $complete}
-
         }
         elseif ($Since) {
             if ($Since -eq 'Today') {
