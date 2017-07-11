f81b788d39e2c6a38c4bd9deb304a47272caaa96
Rob Sewell <mrrobsewell@outlook.com>
2017-07-11 13:52:54 +0100

    added verbose and should process

--- internal/Start-FileUpload.ps1
+++ internal/Start-FileUpload.ps1
@@ -6,25 +6,62 @@
 # URL from Get-UploadURL
 #
 #.PARAMETER File
-# File Path
+# File Object
 ##############################
 function Start-FileUpload {
     [cmdletbinding(SupportsShouldProcess = $true)]
     param([string]$Uri,
-        [string]$File)
-## Needs the Azure.Storage dll file from the Azure.Storage Module
-## Dont know if I can put it in here or it needs to come from the module
+        [object]$File)
+    ## Needs the Azure.Storage dll file from the Azure.Storage Module
+    ## Dont know if I can put it in here or it needs to come from the Azure module
     if (!(Get-Module Azure.Storage -ListAvailable)) {
-        $Base = (Get-Module -Name SQLDiagAPI).ModuleBase
-        Add-Type -Path $Base\internal\Microsoft.WindowsAzure.Storage.dll
+        
+        Write-Verbose -Message "Getting SQLDiagAPI Module Base path"
+        try {
+            if ($PSCmdlet.ShouldProcess("SQLDiagAPI module", "Getting SQLDiagAPI module base path")) { 
+                $Base = (Get-Module -Name SQLDiagAPI).ModuleBase
+            }
+        }
+        catch {
+            Write-Warning -Message "Failed to get  SQLDiagAPI module base path - quitting"
+            break
+        }
+        Write-Verbose -Message "adding Azure Storage dll from SQLDiagAPI module"
+        try {
+            if ($PSCmdlet.ShouldProcess("Azure.Storage Dll", "Loading Assembly")) { 
+                Add-Type -Path $Base\internal\Microsoft.WindowsAzure.Storage.dll
+            }
+        }
+        catch {
+            Write-Warning -Message "Failed to load assembly from SQLDiagAPI module - quitting"
+            break
+        }
     }
     else {
-        $Base = (Get-Module -Name Azure.Storage -ListAvailable).ModuleBase
-        $StorageDll = (Get-ChildItem -Path $Base -Recurse Microsoft.WindowsAzure.Storage.dll).FullName
-        Add-Type $StorageDll
+        Write-Verbose -Message "Getting Azure.Storage Module Base path"
+        try {
+            if ($PSCmdlet.ShouldProcess("Azure.Storage module", "Getting Azure.Storage module base path")) { 
+                $Base = (Get-Module -Name Azure.Storage -ListAvailable).ModuleBase
+            }
+        }
+        catch {
+            Write-Warning -Message "Failed to get  Azure.Storage module base path - quitting"
+            break
+        }
+        Write-Verbose -Message "adding Azure Storage dll from Azure.Storage module"
+        try {
+            if ($PSCmdlet.ShouldProcess("Azure.Storage Dll", "Loading Assemblyfrom Azure.Storage Module")) { 
+                $StorageDll = (Get-ChildItem -Path $Base -Recurse Microsoft.WindowsAzure.Storage.dll).FullName
+                Add-Type $StorageDll
+            }
+        }
+        catch {
+            Write-Warning -Message "Failed to load assembly from Azure.Storage module - quitting"
+            break
+        }
     }
     <#
-    if we could use async upload then use this
+    if we could use async upload then use this but I cannot get it to work
     $blobContent = System.IO.File.ReadAllBytes($file.FullName)
     $blob = New-Object Microsoft.WindowsAzure.Storage.Blob.CloudBlockBlob ($uri)
     $Blob.UploadFromByteArrayAsync($blobContent, 0, $File.Size)
