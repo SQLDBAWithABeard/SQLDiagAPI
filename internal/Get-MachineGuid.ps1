6ad6296657d00f3ed4f7e0e6786bbb77f499cf25
Rob Sewell <mrrobsewell@outlook.com>
2017-07-11 14:49:53 +0100

    should process for machine guid

--- internal/Get-MachineGuid.ps1
+++ internal/Get-MachineGuid.ps1
@@ -1,8 +1,19 @@
 function Get-MachineGUID {
+    ##############################
+    #.SYNOPSIS
+    # Internal function to get hte mcahine guid for identifying the machine to the API
+    #
+    ##############################
+    [cmdletbinding(SupportsShouldProcess = $true)]
+    param()
     try {
-    (Get-ItemProperty registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography\ -Name MachineGuid).MachineGUID
-}
-catch{
+        Write-Verbose -Message "Reading the registry for the machine guid"
+        if ($PSCmdlet.ShouldProcess($Env:COMPUTERNAME, "Reading the registry for the machine guid")) { 
+            (Get-ItemProperty registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography\ -Name MachineGuid).MachineGUID
+            Write-Verbose -Message "Got the machine guid from the registry"
+        }
+    }
+    catch {
         Write-Warning "Failed to get Machine GUID from HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography\"
-}
+    }
 }
