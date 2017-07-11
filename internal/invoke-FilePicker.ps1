473043b530fbfdd58ee99cbcd67ab57366169e67
Rob Sewell <mrrobsewell@outlook.com>
2017-07-11 14:44:44 +0100

    verbose should process

--- internal/invoke-FilePicker.ps1
+++ internal/invoke-FilePicker.ps1
@@ -3,20 +3,67 @@ function Invoke-FilePicker {
     #.SYNOPSIS
     # Internal command to open a file browser and select a mdmp file and return Path and Size in MB
     ##############################
-    Add-Type -AssemblyName System.Windows.Forms
-    $Picker = New-Object System.Windows.Forms.OpenFileDialog -Property @{
-        Multiselect = $false # Multiple files can be chosen
-        Filter      = 'SQL Server Dump File (*.mdmp)|*.mdmp'
+    [cmdletbinding(SupportsShouldProcess = $true)]
+    param()
+    try {
+        Write-Verbose -message "Loading Forms assembly"
+        if ($PSCmdlet.ShouldProcess($env:COMPUTERNAME, "Loading the forms assembly ")) { 
+            Add-Type -AssemblyName System.Windows.Forms
+        }
+    }
+    catch {
+        Write-Warning -Message "Failed to load forms assembly"
+        break
     }
 
-    ShowDialog 
+    try {
+        Write-Verbose -message "Creating the File picker object"
+        if ($PSCmdlet.ShouldProcess($env:COMPUTERNAME, "Creating the File picker object")) { 
+            $Picker = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+                Multiselect = $false # Multiple files can be chosen
+                Filter      = 'SQL Server Dump File (*.mdmp)|*.mdmp'
+            }
+        }
+    }
+    catch {
+        Write-Warning -Message "Failed to create the File Picker Object"
+        break
+    }
+
+    try {
+        Write-Verbose -message "Show the File picker dialogue"
+        if ($PSCmdlet.ShouldProcess($env:COMPUTERNAME, "show the file picker dialogue")) { 
+            ShowDialog 
+        }
+    }
+    catch {
+        Write-Warning -Message "Failed to open File picker dialogue"
+        break
+    }
     
-    $File = Get-Item $Picker.FileName
-    [PSCustomObject] @{
-        Fullname = $File.Fullname
-        Length = $File.Length
+    try {
+        Write-Verbose -message "Getting information about the file $Picker.FullName"
+        if ($PSCmdlet.ShouldProcess($Picker.FullName, "Getting information about the file ")) { 
+            $File = Get-Item $Picker.FileName
+        }
     }
-
+    catch {
+        Write-Warning -Message "Failed to get  information about $($Picker.FullName)"
+        break
+    }
+    try {
+        Write-Verbose -message "Returning the file object"
+        if ($PSCmdlet.ShouldProcess($File.FullName, "Returning the file object ")) { 
+            [PSCustomObject] @{
+                Fullname = $File.Fullname
+                Length   = $File.Length
+            }
+        }
+    }
+    catch {
+        Write-Warning -Message "Failed to return the file object"
+        break
+    }
 }
 
 
