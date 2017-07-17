9f973ed100cb94658baf12dc9384f075284207f9
Rob Sewell <mrrobsewell@outlook.com>
2017-07-17 15:44:09 +0100

    Added GUID from registry for SSMS

--- internal/Get-MachineGuid.ps1
+++ internal/Get-MachineGuid.ps1
@@ -9,7 +9,18 @@ function Get-MachineGUID {
     try {
         Write-Verbose -Message "Reading the registry for the machine guid"
         if ($PSCmdlet.ShouldProcess($Env:COMPUTERNAME, "Reading the registry for the machine guid")) { 
-            (Get-ItemProperty registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography\ -Name MachineGuid).MachineGUID
+            if(Test-Path 'HKLM:\\SOFTWARE\WOW6432Node\Microsoft\Microsoft SQL Sever\Extensions\ECS'){
+            (Get-ItemProperty 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Microsoft SQL Sever\Extensions\ECS' -Name SsmsExtension).SsmsExtension
+                Write-Verbose -Message "Got the SSMS machine guid from the registry at HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Microsoft SQL Sever\Extensions\ECS"    
+    }
+            elseif (Test-Path HKLM:\SOFTWARE\Microsoft\Microsoft SQL Sever\Extensions\ECS\SsmsExtension){
+                (Get-ItemProperty 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Sever\Extensions\ECS\SsmsExtension' -Name SsmsExtension).SsmsExtension
+                Write-Verbose -Message "Got the SSMS machine guid from the registry at HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Sever\Extensions\ECS\SsmsExtension"
+            }
+            else{
+                (Get-ItemProperty registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography\ -Name MachineGuid).MachineGUID
+                Write-Verbose -Message "Got the general machine guid from the registry at HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography\"
+            }
             Write-Verbose -Message "Got the machine guid from the registry"
         }
     }
