8c9d359861da9d77f2a636aff22e0852966dd7d7
Rob Sewell <mrrobsewell@outlook.com>
2017-07-18 08:59:34 +0100

    convert to splat

--- internal/Get-UploadURL.ps1
+++ internal/Get-UploadURL.ps1
@@ -29,7 +29,15 @@ function Get-UploadURL{
         Write-Verbose -Message "Getting the Upload URL for $($File.FullName) with GetUploadURL API $UploadAPIURL"
         if ($PSCmdlet.ShouldProcess('SQL Analysis GetUploadURL', "Getting the Upload URL for $File with $UploadAPIURL")) { 
             try {
-                $UploadResponse = Invoke-RestMethod -Method Post -Uri $UploadAPIURL -Headers $headers -Body $Body -ContentType "application/json"  -ErrorAction Stop
+                $invokeRestMethodSplat = @{
+                    Uri = $UploadAPIURL
+                    ContentType = "application/json"
+                    Headers = $headers
+                    Method = 'Post'
+                    ErrorAction = 'Stop'
+                    Body = $Body
+                }
+                $UploadResponse = Invoke-RestMethod @invokeRestMethodSplat
                 Write-Verbose -Message "Got the Upload URL $UploadResponse"
             }
             catch {
